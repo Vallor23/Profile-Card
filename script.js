78855cc32c9982f3fa7cdaf819951404c6848b29
Vallor23 <enosnyabinda2017@gmail.com>
2025-02-07 12:07:08 +0300

    Renamed script file

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ function updtateTimeUTC () {
     const utcTimeElement = document.querySelector("[data-testid='currentTimeUTC']");
     const date = new Date();
     const utcDate = date.toUTCString();
-    utcTimeElement.textContent = `Current UTC time ${utcDate}`
-}
- updtateTimeUTC();
+    utcTimeElement.textContent = `Current UTC time: ${utcDate}`
+    console.log(utcDate);
+};
+
+updtateTimeUTC();
 setInterval(updtateTimeUTC, 1000);
